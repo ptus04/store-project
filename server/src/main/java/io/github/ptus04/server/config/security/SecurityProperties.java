f4b5bee08d91d60a2bde9e4d54bd340c99cd11a3
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-04-25 04:12:38 +0700

    refactor: separate concerns
    separate cors, password encoder and filter chain

--- server/src/main/java/io/github/ptus04/server/config/security/SecurityProperties.java
+++ server/src/main/java/io/github/ptus04/server/config/security/SecurityProperties.java
@@ -1,17 +1,27 @@
 package io.github.ptus04.server.config.security;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.util.List;
 
-@ConfigurationProperties(prefix = "custom.security")
+@RequiredArgsConstructor
 @Getter
 @Setter
-public class SecurityProperties {
-    private List<String> allowedOrigins = List.of();
-    private List<String> allowedMethods = List.of();
-    private List<String> allowedHeaders = List.of();
-    private boolean allowCredentials = false;
+@ConfigurationProperties(prefix = "custom.security")
+public final class SecurityProperties {
+    @NestedConfigurationProperty
+    private final CorsProperties cors = new CorsProperties();
+
+    @Getter
+    @Setter
+    public static final class CorsProperties {
+        private List<String> allowedOrigins = List.of();
+        private List<String> allowedMethods = List.of();
+        private List<String> allowedHeaders = List.of();
+        private boolean allowCredentials = false;
+    }
 }
