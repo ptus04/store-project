d3080ed0ca7e2439a79e1208c1643ccfbc522df3
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-04-25 03:25:38 +0700

    fix: default error pages do not work

--- server/src/main/java/io/github/ptus04/server/controller/advice/GlobalControllerAdvice.java
+++ server/src/main/java/io/github/ptus04/server/controller/advice/GlobalControllerAdvice.java
@@ -1,13 +1,7 @@
 package io.github.ptus04.server.controller.advice;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalControllerAdvice {
-
-    @ExceptionHandler(Exception.class)
-    public Exception handleException(Exception e) {
-        return e;
-    }
 }
