a7ed17186f748bbd115680bd938954232e1dfe76
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-04-29 16:41:39 +0700

    feat: enable caching and redis repository mode

---
+++ server/src/main/java/io/github/ptus04/server/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package io.github.ptus04.server.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
