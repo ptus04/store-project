1021a1defc831aa521a5dbe3aca48fe7935ad18c
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-04-29 17:10:02 +0700

    feat: enable Async support and virtual threads

--- server/src/main/java/io/github/ptus04/server/ServerApplication.java
+++ server/src/main/java/io/github/ptus04/server/ServerApplication.java
@@ -3,8 +3,10 @@ package io.github.ptus04.server;
 import com.azure.spring.cloud.autoconfigure.implementation.storage.queue.AzureStorageQueueAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = AzureStorageQueueAutoConfiguration.class)
+@EnableAsync
 public class ServerApplication {
 
     public static void main(String[] args) {
