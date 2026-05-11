0cafaf70d3af9ca0eb5deded855586bfbb2fe076
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-11 21:54:16 +0700

    change links to English

--- server/src/main/java/io/github/ptus04/server/security/SecurityConfig.java
+++ server/src/main/java/io/github/ptus04/server/security/SecurityConfig.java
@@ -42,23 +42,33 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/img/**", "/favicon.png").permitAll()
-                        .requestMatchers("/", "/danh-sach-san-pham/**", "/gio-hang").permitAll()
-                        .requestMatchers("/dang-nhap/**", "/dang-ky/**").permitAll()
-                        .requestMatchers("/about-us", "/privacy-policy", "/refund-policy").permitAll()
-                        .requestMatchers("/error").permitAll()
-                        .requestMatchers("/actuator/**").permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers(
+                                "/",
+                                "/about-us",
+                                "/privacy-policy",
+                                "/refund-policy",
+                                "/error",
+                                "/actuator/**",
+                                "/auth/**",
+                                "/products/**",
+                                "/cart/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin(form -> form
-                        .loginPage("/dang-nhap")
-                        .loginProcessingUrl("/dang-nhap")
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .defaultSuccessUrl("/")
                         .failureHandler(customAuthFailureHandler)
-                        .defaultSuccessUrl("/"))
+                )
                 .logout(logout -> logout
-                        .logoutUrl("/dang-xuat")
-                        .logoutSuccessUrl("/dang-nhap"))
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login")
+                )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                        .maximumSessions(1));
+                        .maximumSessions(1)
+                );
 
         return http.build();
     }
--- server/src/main/java/io/github/ptus04/server/service/AuthService.java
+++ server/src/main/java/io/github/ptus04/server/service/AuthService.java
@@ -1,12 +1,12 @@
 package io.github.ptus04.server.service;
 
 import io.github.ptus04.server.dto.request.RegistrationRequest;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
 
 import java.util.UUID;
 
 public interface AuthService {
-    void register(HttpSession httpSession, RegistrationRequest registrationRequest);
+    Authentication register(RegistrationRequest registrationRequest);
 
     long sendPhoneOtp(UUID userId);
 
