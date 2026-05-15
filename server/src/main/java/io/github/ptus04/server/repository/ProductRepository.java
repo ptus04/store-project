d4a3169885ec047b05d62c2865c6eeae5f16c363
Lê Tấn Phong <141659740+LeTanPhong3003@users.noreply.github.com>
2026-05-15 16:06:59 +0700

    search v1

--- server/src/main/java/io/github/ptus04/server/repository/ProductRepository.java
+++ server/src/main/java/io/github/ptus04/server/repository/ProductRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
     @EntityGraph("Product.withProductImages")
     List<Product> findByIsNew(Boolean isNew);
 
