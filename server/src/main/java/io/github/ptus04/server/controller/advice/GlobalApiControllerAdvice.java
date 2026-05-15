4945a351b9a46638046909dd3c740fc03bcbddae
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-15 22:31:53 +0700

    handle exception more detail

--- server/src/main/java/io/github/ptus04/server/controller/advice/GlobalApiControllerAdvice.java
+++ server/src/main/java/io/github/ptus04/server/controller/advice/GlobalApiControllerAdvice.java
@@ -3,37 +3,85 @@ package io.github.ptus04.server.controller.advice;
 import io.github.ptus04.server.dto.response.ApiErrorResponse;
 import io.github.ptus04.server.exception.ProductNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalApiControllerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<?> handleNotFound(ProductNotFoundException ex, HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+    public ResponseEntity<?> handleProductNotFoundException(ProductNotFoundException ex, HttpServletRequest request) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                request.getRequestURI(),
+                "PRODUCT_NOT_FOUND"
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiErrorResponse);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex, HttpServletRequest request) {
+        Map<Object, String> fieldErrors = ex.getConstraintViolations()
+                .stream()
+                .collect(Collectors.toMap(
+                                ConstraintViolation::getPropertyPath,
+                                ConstraintViolation::getMessage,
+                                (a, b) -> a
+                        )
+                );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ApiErrorResponse(
-                        HttpStatus.NOT_FOUND.value(),
-                        HttpStatus.NOT_FOUND.getReasonPhrase(),
-                        ex.getMessage(),
-                        request.getRequestURI(),
-                        System.currentTimeMillis(),
-                        "PRODUCT_NOT_FOUND")
+                                HttpStatus.BAD_REQUEST,
+                                ex.getMessage(),
+                                request.getRequestURI(),
+                                "VALIDATION_ERROR",
+                                fieldErrors
+                        )
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        Map<Object, String> fieldErrors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                                FieldError::getField,
+                                FieldError::getDefaultMessage,
+                                (a, b) -> a
+                        )
+                );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiErrorResponse(
+                                HttpStatus.BAD_REQUEST,
+                                ex.getMessage(),
+                                request.getRequestURI(),
+                                "VALIDATION_ERROR",
+                                fieldErrors
+                        )
                 );
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<?> handleException(Exception ex, HttpServletRequest request) {
-        return ResponseEntity.internalServerError()
-                .body(new ApiErrorResponse(
-                        HttpStatus.NOT_FOUND.value(),
-                        HttpStatus.NOT_FOUND.getReasonPhrase(),
-                        ex.getMessage(),
-                        request.getRequestURI(),
-                        System.currentTimeMillis(),
-                        "INTERNAL_SERVER_ERROR")
-                );
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                request.getRequestURI(),
+                HttpStatus.INTERNAL_SERVER_ERROR.name()
+        );
+        return ResponseEntity.internalServerError().body(apiErrorResponse);
     }
 }
