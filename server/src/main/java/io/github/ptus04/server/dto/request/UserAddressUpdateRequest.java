c14b9f4d51aa7118872934f01107463dab38da9c
Chituongnguyen24 <chituong.dev@gmail.com>
2026-05-18 22:48:08 +0700

    Cập chật  UserAddressUpdateRequest

---
+++ server/src/main/java/io/github/ptus04/server/dto/request/UserAddressUpdateRequest.java
@@ -0,0 +1,19 @@
+package io.github.ptus04.server.dto.request;
+
+import jakarta.validation.constraints.Size;
+
+public record UserAddressUpdateRequest(
+    @Size(max = 32, message = "Tỉnh/Thành phố không được vượt quá 32 ký tự")
+    String city,
+
+    @Size(max = 32, message = "Quận/Huyện không được vượt quá 32 ký tự")
+    String district,
+
+    @Size(max = 32, message = "Phường/Xã không được vượt quá 32 ký tự")
+    String ward,
+
+    @Size(max = 64, message = "Địa chỉ chi tiết không được vượt quá 64 ký tự")
+    String address,
+
+    Boolean isDefault
+) {}
