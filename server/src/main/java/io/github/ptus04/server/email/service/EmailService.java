e3f5e97f494bbab68ac7658b6b2e9dcfa7359846
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-28 21:01:23 +0700

    Resolve conflicts
    TODO: Use MQ for sending email otp

--- server/src/main/java/io/github/ptus04/server/email/service/EmailService.java
+++ server/src/main/java/io/github/ptus04/server/email/service/EmailService.java
@@ -2,4 +2,8 @@ package io.github.ptus04.server.email.service;
 
 public interface EmailService {
     void sendInvoiceEmail(String toEmail, String orderCode, String invoiceLink);
+
+    long sendEmailVerificationOtp(String email);
+
+    boolean verifyEmailOtp(String email, String otp);
 }
