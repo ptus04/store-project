eb55741e98718d861108b7fdcd5842e6e06dbbe2
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-29 09:46:58 +0700

    remove unnecessary repositories

--- server/src/main/java/io/github/ptus04/server/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.ptus04.server.repository;
-
-import io.github.ptus04.server.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, UUID> {
-}
