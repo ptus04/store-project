84bd3429644ead4f85c2073bf366edf593c06fcd
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-29 10:49:26 +0700

    fix missing bean

--- server/src/main/java/io/github/ptus04/server/consumer/SmsEventConsumer.java
+++ server/src/main/java/io/github/ptus04/server/consumer/SmsEventConsumer.java
@@ -2,14 +2,17 @@ package io.github.ptus04.server.consumer;
 
 import com.twilio.rest.verify.v2.service.Verification;
 import io.github.ptus04.server.config.RabbitMQConfig;
+import io.github.ptus04.server.config.TwilioConfig;
 import io.github.ptus04.server.config.TwilioProperties;
 import io.github.ptus04.server.event.OtpRequestedEvent;
 import io.github.ptus04.server.service.SMSVerificationService;
 import io.github.ptus04.server.util.PhoneNumberUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnBean(TwilioConfig.class)
 @Component
 @RequiredArgsConstructor
 public class SmsEventConsumer {
