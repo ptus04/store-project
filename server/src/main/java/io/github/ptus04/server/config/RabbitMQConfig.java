b1c45a59fa510f61f3796e35cd0a539eabbe681f
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-05-29 10:15:08 +0700

    add MQ to sms otp

--- server/src/main/java/io/github/ptus04/server/config/RabbitMQConfig.java
+++ server/src/main/java/io/github/ptus04/server/config/RabbitMQConfig.java
@@ -12,18 +12,22 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
     public static final String QUEUE_CREATE_INVOICE = "invoice.create-invoice";
     public static final String QUEUE_SEND_INVOICE = "email.send-invoice";
+    public static final String QUEUE_SEND_OTP = "sms.send-otp";
 
     public static final String EXCHANGE_INVOICES = "invoices.topic";
     public static final String EXCHANGE_ORDERS = "orders.topic";
+    public static final String EXCHANGE_SMS = "sms.topic";
 
     public static final String KEY_INVOICE_CREATED = "invoice.created";
     public static final String KEY_ORDER_PAID = "order.paid";
+    public static final String KEY_SMS_OTP_REQUESTED = "sms.otp-requested";
 
     @Bean
     public Declarables queues() {
         return new Declarables(
                 QueueBuilder.durable(QUEUE_CREATE_INVOICE).build(),
-                QueueBuilder.durable(QUEUE_SEND_INVOICE).build()
+                QueueBuilder.durable(QUEUE_SEND_INVOICE).build(),
+                QueueBuilder.durable(QUEUE_SEND_OTP).build()
         );
     }
 
@@ -31,7 +35,8 @@ public class RabbitMQConfig {
     public Declarables exchanges() {
         return new Declarables(
                 ExchangeBuilder.topicExchange(EXCHANGE_ORDERS).durable(true).build(),
-                ExchangeBuilder.topicExchange(EXCHANGE_INVOICES).durable(true).build()
+                ExchangeBuilder.topicExchange(EXCHANGE_INVOICES).durable(true).build(),
+                ExchangeBuilder.topicExchange(EXCHANGE_SMS).durable(true).build()
         );
     }
 
@@ -39,7 +44,8 @@ public class RabbitMQConfig {
     public Declarables amqpBindings() {
         return new Declarables(
                 BindingBuilder.bind(new Queue(QUEUE_CREATE_INVOICE)).to(new TopicExchange(EXCHANGE_ORDERS)).with(KEY_ORDER_PAID),
-                BindingBuilder.bind(new Queue(QUEUE_SEND_INVOICE)).to(new TopicExchange(EXCHANGE_INVOICES)).with(KEY_INVOICE_CREATED)
+                BindingBuilder.bind(new Queue(QUEUE_SEND_INVOICE)).to(new TopicExchange(EXCHANGE_INVOICES)).with(KEY_INVOICE_CREATED),
+                BindingBuilder.bind(new Queue(QUEUE_SEND_OTP)).to(new TopicExchange(EXCHANGE_SMS)).with(KEY_SMS_OTP_REQUESTED)
         );
     }
 
